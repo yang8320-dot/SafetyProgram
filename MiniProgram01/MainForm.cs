c4d7786d72a279548419466c86dda2f5f01de6fd
yang8320-dot <yang8320@gmail.com>
2026-03-30 08:46:36 +0800

    Update MainForm.cs

--- MiniProgram01/MainForm.cs
+++ MiniProgram01/MainForm.cs
@@ -12,14 +12,13 @@ public class MainForm : Form {
     private string appName = "MiniProgram01";
     private static Color BgColor = Color.FromArgb(245, 245, 247); 
 
-    // --- 快捷鍵相關 P/Invoke ---
     [DllImport("user32.dll")]
     private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
     [DllImport("user32.dll")]
     private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
     private const int HOTKEY_ID = 9000;
     private const uint MOD_ALT = 0x0001;
-    private const uint VK_1 = 0x31; // '1' 鍵
+    private const uint VK_1 = 0x31; 
     private const int WM_HOTKEY = 0x0312;
 
     public MainForm() {
@@ -35,7 +34,6 @@ public class MainForm : Form {
         Rectangle area = Screen.PrimaryScreen.WorkingArea;
         this.Location = new Point(area.Right - this.Width - 15, area.Bottom - this.Height - 15);
 
-        // 註冊預設快捷鍵 Alt + 1
         RegisterHotKey(this.Handle, HOTKEY_ID, MOD_ALT, VK_1);
 
         trayIcon = new NotifyIcon() { Icon = SystemIcons.Information, Visible = true, Text = "整合通知中心" };
@@ -48,47 +46,53 @@ public class MainForm : Form {
 
         // 【模組 1】檔案監控
         TabPage tabWatcher = new TabPage("📁 檔案監控");
-        tabWatcher.Controls.Add(new App_FileWatcher(this, trayMenu) { Dock = DockStyle.Fill });
+        App_FileWatcher watcherApp = new App_FileWatcher(this, trayMenu);
+        watcherApp.Dock = DockStyle.Fill;
+        tabWatcher.Controls.Add(watcherApp);
         tabControl.TabPages.Add(tabWatcher);
 
         // 【模組 2】待辦事項
         TabPage tabTodo = new TabPage("📝 待辦");
         App_TodoList todoApp = new App_TodoList(this);
-        tabTodo.Controls.Add(todoApp { Dock = DockStyle.Fill });
+        todoApp.Dock = DockStyle.Fill; // 【修正點 1】分開寫避免語法錯誤
+        tabTodo.Controls.Add(todoApp);
         tabControl.TabPages.Add(tabTodo);
 
         // 【模組 3】週期任務
-        TabPage tabRecurring = new TabPage("🔁 週期任務");
-        tabRecurring.Controls.Add(new App_RecurringTasks(this, todoApp) { Dock = DockStyle.Fill });
+        TabPage tabRecurring = new TabPage("🔁 週期");
+        App_RecurringTasks recurringApp = new App_RecurringTasks(this, todoApp);
+        recurringApp.Dock = DockStyle.Fill;
+        tabRecurring.Controls.Add(recurringApp);
         tabControl.TabPages.Add(tabRecurring);
 
-        // 【全新模組 4】捷徑管理
+        // 【模組 4】捷徑管理
         TabPage tabShortcuts = new TabPage("🚀 捷徑");
-        tabShortcuts.Controls.Add(new App_Shortcuts(this) { Dock = DockStyle.Fill });
+        App_Shortcuts shortcutsApp = new App_Shortcuts(this);
+        shortcutsApp.Dock = DockStyle.Fill;
+        tabShortcuts.Controls.Add(shortcutsApp);
         tabControl.TabPages.Add(tabShortcuts);
 
         trayMenu.MenuItems.Add("-");
-        MenuItem startupMenu = new MenuItem("開機自動執行") { Checked = IsRunOnStartup() };
+        MenuItem startupMenu = new MenuItem("開機自動執行");
+        startupMenu.Checked = IsRunOnStartup();
         startupMenu.Click += new EventHandler(ToggleStartup);
         trayMenu.MenuItems.Add(startupMenu);
         trayMenu.MenuItems.Add("結束程式", new EventHandler(delegate { UnregisterHotKey(this.Handle, HOTKEY_ID); trayIcon.Dispose(); Application.Exit(); }));
         trayIcon.ContextMenu = trayMenu;
 
         this.Opacity = 0; 
-        this.Load += (s, e) => { this.Hide(); this.Opacity = 1; };
+        this.Load += new EventHandler(delegate { this.Hide(); this.Opacity = 1; });
     }
 
     public void ShowAppWindow(int targetTabIndex = -1) {
         this.Show();
         if (this.WindowState == FormWindowState.Minimized) this.WindowState = FormWindowState.Normal;
-        if (targetTabIndex >= 0) tabControl.SelectedIndex = targetTabIndex; // 切換分頁
+        if (targetTabIndex >= 0 && targetTabIndex < tabControl.TabCount) tabControl.SelectedIndex = targetTabIndex; 
         this.Activate(); 
     }
 
     protected override void WndProc(ref Message m) {
-        if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID) {
-            ShowAppWindow(1); // 按下快捷鍵，呼叫程式並跳到「待辦(Index 1)」
-        }
+        if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID) { ShowAppWindow(1); }
         base.WndProc(ref m);
     }
 
@@ -98,11 +102,21 @@ public class MainForm : Form {
     }
 
     private bool IsRunOnStartup() {
-        try { using (RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", false)) { return rk?.GetValue(appName) != null; } } catch { return false; }
+        try { 
+            using (RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", false)) { 
+                // 【修正點 2】不使用 ?. 運算子，改用傳統 null 檢查確保絕對相容
+                if (rk != null && rk.GetValue(appName) != null) return true;
+            } 
+        } catch { } return false;
     }
 
     private void ToggleStartup(object sender, EventArgs e) {
         MenuItem item = (MenuItem)sender; item.Checked = !item.Checked;
-        try { using (RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true)) { if (item.Checked) rk.SetValue(appName, "\"" + Application.ExecutablePath + "\""); else rk.DeleteValue(appName, false); } } catch {}
+        try { 
+            using (RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true)) { 
+                if (item.Checked) rk.SetValue(appName, "\"" + Application.ExecutablePath + "\""); 
+                else if (rk != null) rk.DeleteValue(appName, false); 
+            } 
+        } catch {}
     }
 }
