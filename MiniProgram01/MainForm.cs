0bf219721e2205b2116175bb61812adad850dab3
yang8320-dot <yang8320@gmail.com>
2026-03-31 07:49:50 +0800

    Update MainForm.cs

--- MiniProgram01/MainForm.cs
+++ MiniProgram01/MainForm.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Windows.Forms;
 using Microsoft.Win32;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
 public class MainForm : Form {
     public NotifyIcon trayIcon;
@@ -11,13 +12,18 @@ public class MainForm : Form {
     private string appName = "MiniProgram01";
     private static Color BgColor = Color.FromArgb(245, 245, 247); 
 
+    // 閃動提醒相關變數
+    private HashSet<int> alertTabs = new HashSet<int>();
+    private Timer flashTimer;
+    private bool flashState = false;
+
     [DllImport("user32.dll")]
     private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
     [DllImport("user32.dll")]
     private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
     private const int HOTKEY_ID = 9000;
-    private const uint MOD_CONTROL = 0x0002; // 【修正】改為 Ctrl
+    private const uint MOD_CONTROL = 0x0002; 
     private const uint VK_1 = 0x31; 
     private const int WM_HOTKEY = 0x0312;
 
@@ -32,7 +38,6 @@ public class MainForm : Form {
         Rectangle area = Screen.PrimaryScreen.WorkingArea;
         this.Location = new Point(area.Right - this.Width - 15, area.Bottom - this.Height - 15);
 
-        // 註冊 Ctrl + 1
         RegisterHotKey(this.Handle, HOTKEY_ID, MOD_CONTROL, VK_1);
 
         trayIcon = new NotifyIcon() { Icon = SystemIcons.Information, Visible = true, Text = "整合通知中心" };
@@ -40,8 +45,29 @@ public class MainForm : Form {
         trayMenu.MenuItems.Add("顯示主面板", new EventHandler(delegate { ShowAppWindow(); }));
         trayMenu.MenuItems.Add("-");
 
-        tabControl = new TabControl() { Dock = DockStyle.Fill, Padding = new Point(12, 5), Font = new Font("Microsoft JhengHei UI", 9f, FontStyle.Bold) };
+        // 【修正】開啟自行繪製 Tab 模式以支援閃爍
+        tabControl = new TabControl() { 
+            Dock = DockStyle.Fill, Padding = new Point(12, 5), 
+            Font = new Font("Microsoft JhengHei UI", 9f, FontStyle.Bold),
+            DrawMode = TabDrawMode.OwnerDrawFixed // 允許自訂繪圖
+        };
+        tabControl.DrawItem += TabControl_DrawItem;
+        tabControl.SelectedIndexChanged += (s, e) => {
+            if (alertTabs.Contains(tabControl.SelectedIndex)) {
+                alertTabs.Remove(tabControl.SelectedIndex);
+                tabControl.Invalidate();
+            }
+        };
         this.Controls.Add(tabControl);
+
+        // 啟動閃爍計時器
+        flashTimer = new Timer() { Interval = 500, Enabled = true };
+        flashTimer.Tick += (s, e) => {
+            if (alertTabs.Count > 0) {
+                flashState = !flashState;
+                tabControl.Invalidate(); 
+            }
+        };
 
         // 載入模組
         TabPage tabWatcher = new TabPage("📁 監控");
@@ -79,16 +105,37 @@ public class MainForm : Form {
         this.Load += new EventHandler(delegate { this.Hide(); this.Opacity = 1; });
     }
 
+    // 【新增 API】呼叫此方法可讓指定的分頁閃爍
+    public void AlertTab(int tabIndex) {
+        if (tabControl.SelectedIndex != tabIndex && tabIndex >= 0 && tabIndex < tabControl.TabCount) {
+            alertTabs.Add(tabIndex);
+        }
+    }
+
+    // 自繪分頁邏輯 (處理閃爍顏色)
+    private void TabControl_DrawItem(object sender, DrawItemEventArgs e) {
+        Graphics g = e.Graphics;
+        TabPage page = tabControl.TabPages[e.Index];
+        Rectangle bounds = tabControl.GetTabRect(e.Index);
+
+        bool isAlert = alertTabs.Contains(e.Index) && flashState;
+        bool isSelected = (e.State & DrawItemState.Selected) == DrawItemState.Selected;
+
+        Color bgColor = isAlert ? Color.FromArgb(255, 180, 180) : (isSelected ? Color.White : Color.FromArgb(240, 240, 240));
+        Color textColor = isSelected ? Color.FromArgb(0, 122, 255) : Color.Black;
+
+        using (Brush b = new SolidBrush(bgColor)) { g.FillRectangle(b, bounds); }
+        TextRenderer.DrawText(g, page.Text, page.Font, bounds, textColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+    }
+
     public void ShowAppWindow(int targetTabIndex = -1) {
         this.Show();
         if (this.WindowState == FormWindowState.Minimized) this.WindowState = FormWindowState.Normal;
-        // 【修正】只有當明確傳入 index 時才切換，否則保持現狀
         if (targetTabIndex >= 0 && targetTabIndex < tabControl.TabCount) tabControl.SelectedIndex = targetTabIndex; 
         this.Activate(); 
     }
 
     protected override void WndProc(ref Message m) {
-        // 【修正】快捷鍵觸發時，不傳入參數，維持目前頁面
         if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID) { ShowAppWindow(); }
         base.WndProc(ref m);
     }
