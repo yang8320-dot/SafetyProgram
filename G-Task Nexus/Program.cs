66d471308d034cbf63f42054f0eaf9ad42ed8ed9
yang8320-dot <yang8320@gmail.com>
2026-04-02 14:32:27 +0800

    Update Program.cs

--- G-Task Nexus/Program.cs
+++ G-Task Nexus/Program.cs
@@ -10,7 +10,6 @@ namespace GTaskNexus
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            // 啟動主程式
             Application.Run(new MainForm());
         }
     }
