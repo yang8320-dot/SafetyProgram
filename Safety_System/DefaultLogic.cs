86c80aa08d808cf9c2b82efaf772161b386a106c
yang8320-dot <yang8320@gmail.com>
2026-04-28 23:33:06 +0800

    Create DefaultLogic.cs

---
+++ Safety_System/DefaultLogic.cs
@@ -0,0 +1,34 @@
+/// FILE: Safety_System/DefaultLogic.cs ///
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Safety_System
+{
+    public class DefaultLogic : ITableLogic
+    {
+        public virtual void InitializeSchema(string dbName, string tableName) { }
+
+        public virtual Task<bool> OnBeforeSaveAsync(string dbName, string tableName, DataTable savingData)
+        {
+            return Task.FromResult(true); // 預設無須預處理，直接放行
+        }
+
+        public virtual Task OnAfterSaveAsync(string dbName, string tableName, DataTable savedData)
+        {
+            return Task.CompletedTask; // 預設無背景任務
+        }
+
+        public virtual string[] GetDropdownList(string tableName, string columnName)
+        {
+            return TableSchemaManager.GetDropdownList(tableName, columnName);
+        }
+
+        public virtual string[] GetDependentDropdownList(string tableName, string columnName, string parentValue)
+        {
+            return TableSchemaManager.GetDependentDropdownList(tableName, columnName, parentValue);
+        }
+
+        public virtual void OnCellClick(DataGridView dgv, DataGridViewCellEventArgs e) { }
+    }
+}
