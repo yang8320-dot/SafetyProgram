4abc9e458f1afd9b879c5144d6658a56f049d6cb
yang8320-dot <yang8320@gmail.com>
2026-05-01 22:01:11 +0800

    Update ITableLogic.cs

--- Safety_System/ITableLogic.cs
+++ Safety_System/ITableLogic.cs
@@ -1,4 +1,3 @@
-/// FILE: Safety_System/ITableLogic.cs ///
 using System;
 using System.Data;
 using System.Threading.Tasks;
@@ -9,8 +8,8 @@ namespace Safety_System
     public interface ITableLogic
     {
         void InitializeSchema(string dbName, string tableName);
-        // 🟢 加入進度條參數
-        Task<bool> OnBeforeSaveAsync(string dbName, string tableName, DataTable savingData, IProgress<string> progressStr = null);
+        // 🟢 加入 IProgress<int> 以支援進度條回報
+        Task<bool> OnBeforeSaveAsync(string dbName, string tableName, DataTable savingData, IProgress<int> progressInt = null, IProgress<string> progressStr = null);
         Task OnAfterSaveAsync(string dbName, string tableName, DataTable savedData);
         string[] GetDropdownList(string tableName, string columnName);
         string[] GetDependentDropdownList(string tableName, string columnName, string parentValue);
