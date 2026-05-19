5b3bb6b5f19d91cf1fc67e4fb55a00080cb80433
yang8320-dot <yang8320@gmail.com>
2026-05-19 17:11:13 +0800

    Create ITableLogic.cs

---
+++ Safety_System/CoreTable/ITableLogic.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Safety_System
+{
+    public interface ITableLogic
+    {
+        void InitializeSchema(string dbName, string tableName);
+        Task<bool> OnBeforeSaveAsync(string dbName, string tableName, DataTable savingData, IProgress<int> progressInt = null, IProgress<string> progressStr = null);
+        Task OnAfterSaveAsync(string dbName, string tableName, DataTable savedData);
+        string[] GetDropdownList(string tableName, string columnName);
+        string[] GetDependentDropdownList(string tableName, string columnName, string parentValue);
+        void OnCellClick(DataGridView dgv, DataGridViewCellEventArgs e);
+    }
+}
